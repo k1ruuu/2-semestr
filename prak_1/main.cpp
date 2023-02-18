689876a20d048e46202d81eaed6fecd481703d1c
k1ruuu <113135708+k1ruuu@users.noreply.github.com>
2023-02-18 10:53:32 +0200

    Add files via upload

--- prak_1/main.cpp
+++ prak_1/main.cpp
@@ -10,39 +10,82 @@ class shifr {
 private:
 	vector <int> fib = { 1, 2 };
 	vector <string> text;
+	vector <string> code;
 public:
+	shifr() {
+		code.resize(30);
+	}
 	void fibanachi(int i) {
 		for (int j = 2; j < i; j++) {
 			fib.push_back(fib[j - 1] + fib[j - 2]);
 		}
 	}
-	void encode(){
-		string str;
-		getline(cin, str);
-		string txt = "";
-		for (int i = 0; i < str.length(); i++) {
-			if (str[i] != ' ') {
-				txt += str[i];
-			}
-			if (str[i] == ' ') {
-				text.push_back(txt);
-				txt = "";
-			}
+	void encode(vector <int> key){
+		if (check(key) == false) {
+			cout << "Ошибка в ключе";
 		}
-		if (txt.length() != 0) {
-			text.push_back(txt);
+		else {
+			string str;
+			getline(cin, str);
+			string txt = "";
+			for (int i = 0; i < str.length(); i++) {
+				if (str[i] != ' ') {
+					txt += str[i];
+				}
+				if (str[i] == ' ') {
+					text.push_back(txt);
+					txt = "";
+				}
+			}
+			if (txt.length() != 0) {
+				text.push_back(txt);
+			}
+			for (int i = 0; i < key.size(); i++) {
+				for (int j = 0; j < fib.size(); j++) {
+					if (key[i] == fib[j]) {
+						code[j] = text[i];
+					}
+				}
+			}
 		}
 	}
-	void get() {
-		for (int i = 0; i < text.size(); i++) {
-			cout << text[i] << " ";
+	void decode(vector <int> key) {
+		if (text.size() > 30) {
+			cout << "Много слов" << endl;
 		}
+		if (check(key) == false) {
+			cout << "Ошибка в ключе";
+		}
+		else {
+			for (int i = 0; i < key.size(); i++) {
+				for (int j = 0; j < fib.size(); j++) {
+					if (key[i] == fib[j]) {
+						cout << code[j];
+					}
+				}
+			}
+		}
+	}
+	bool check(vector <int> mas) {
+		for (int i = 0; i < mas.size(); i++) {
+			int count = 0;
+			for (int j = 0; j < fib.size(); j++) {
+				if (mas[i] != fib[j]) {
+					count++;
+				}
+			}
+			if (count == fib.size()) {
+				return false;
+			}
+		}
+		return true;
 	}
 };
 
 int main() {
+	setlocale(LC_ALL, "rus");
 	shifr i;
 	i.fibanachi(30);
-	i.encode();
-	i.get();
+	i.encode({3,2,1,8,5});
+	i.decode({ 1,3,2,8,4 });
 }
